819d26e125708443208a133df8732be66e1aa452
Joel Rivera <rivera@joel.mx>
2018-03-21 00:42:03 -0600

    Add test release.nix to be built on hydra.

---
+++ release.nix
@@ -0,0 +1,22 @@
+{ nixpkgs
+, bbb
+, masterConfig
+, externalMasterDir
+}:
+let 
+  masterConfigDrv = with (import nixpkgs {});
+  stdenv.mkDerivation {
+     name = "master-config";
+     src = copyPathToStore masterConfig;
+     phases = [ "buildPhase" ];
+     buildPhase = ''
+       mkdir $out
+       cp -r  $src/* $out/
+       substituteInPlace $out/config.json --subst-var-by NIX_STORE_PATH $out
+     '';
+  };
+  masterConfigFile = "${masterConfigDrv}/config.json";
+  bolted-buildbot = import bbb { inherit nixpkgs  masterConfigFile externalMasterDir; };
+in {
+  inherit (bolted-buildbot) lw-master;
+}
